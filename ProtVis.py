776330f2a189ea57667d1e8268558f4aa4fceecd
tocssharma <37792729+tocssharma@users.noreply.github.com>
2021-10-15 11:29:31 +0530

    Create ProtVis.py

---
+++ ProtVis.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import streamlit.components.V1 as components
+id=st.text_input("Enter the PDB Id:")
+if id:
+  components.html(
+    """ <script src="https://3Dmol.org/build/3Dmol.js" async></script>
+        <div style="height: 400px; width: 400px; position: relative;"
+        class='viewer_3Dmol.js' darta-pdb={}
+        data-style1='cartoon:color=spectrum' data-spin='axis:y;speed:3>>/div>
+    """.format(id),height=800,
+    )
